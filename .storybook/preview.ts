6eadaa3f33f2dcd0e4fe373aec8082dbd2578ca5
Pedro <pedrovenegasjob@gmail.com>
2023-05-20 13:46:48 +0200

    commit 03

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,23 +1,24 @@
-// import type { Preview } from "@storybook/react";
-// import '../stories/globals.css'
-// const preview: Preview = {
-//   parameters: {
-//     actions: { argTypesRegex: "^on[A-Z].*" },
-//     controls: {
-//       matchers: {
-//         color: /(background|color)$/i,
-//         date: /Date$/,
-//       },
-//     },
-//   },
-// };
-
-// export default preview;
-
+import type { Preview } from "@storybook/react";
 
 import "../stories/globals.css";
 
 import * as nextImage from "next/image";
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
+
+
+
 
 Object.defineProperty(nextImage, "default", {
   configurable: true,
